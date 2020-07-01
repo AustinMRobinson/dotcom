3159827b752955e9825ff5b05fb35ddeb35d207c
Austin Robinson <austinrobinsondesign@gmail.com>
2020-07-01 00:06:54 -0500

    Added profile picture

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,14 @@ module.exports = {
     'gatsby-plugin-dark-mode',
     `gatsby-plugin-emotion`,
     `gatsby-plugin-postcss`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/images/`,
+      },
+    },
     `gatsby-plugin-react-helmet`,
   ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,8 @@
 import tw from "twin.macro"
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
+
 import SEO from "../components/seo"
 import styled from "@emotion/styled"
 import { ThemeToggler } from 'gatsby-plugin-dark-mode'
@@ -64,10 +67,10 @@ const Intro = styled.section`
       ${tw`w-full lg:w-2/3 flex flex-col space-y-2 lg:space-y-4`}
     }
     .trailing {
-      ${tw`w-full lg:w-1/3 h-64 mb-8 lg:mb-0`}
+      ${tw`w-1/2 lg:w-1/3 h-48 md:h-64 mb-8 lg:mb-0 rounded-lg overflow-hidden`}
       background: var(--bg3);
       img {
-        ${tw`w-full h-full rounded-lg`}
+        ${tw`w-full h-full object-cover`}
       }
     }
   }
@@ -129,110 +132,127 @@ const Button = styled.button`
   background: var(--foreground);
 `
 
-export default () => (
-  <ThemeToggler>
-    {() => (
-    <Wrapper>
-      <SEO title="Home" />
-      <Main>
+const Index = () => {
 
-        <Header>
-          <Container>
-            <nav>
-              <NavLogo href="#">
-                <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M26.75 0H2C0.89543 0 0 0.89543 0 2V26.5625H26.75C34.0625 26.5625 40 20.625 40 13.3125C40 6 34.0625 0 26.75 0Z"/>
-                  <path d="M0 2.85723V38.0001C0 39.1046 0.895431 40.0001 2 40.0001H36.9822C38.0959 40.0001 38.6536 38.6536 37.8661 37.8662L20 20.0001L0.58 0.590088V0.590088C0.209346 0.947905 0 1.44098 0 1.95616V2.85723Z"/>
-                </svg>
-              </NavLogo>
-              <NavMenu>
-                <a href="#">Testing</a>
-                <a href="#">Testing</a>
-                <a href="#">Testing</a>
-              </NavMenu>
-            </nav>
-          </Container>
-        </Header>
+  const data = useStaticQuery(graphql`
+    query {
+      file(relativePath: {eq: "prof-pic.jpg"}) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
 
-        <Intro>
-          <Container>
-            <div className="intro-wrapper">
-              <div className="leading">
-                <TitleLg>Hi, I’m Austin</TitleLg>
-                <SubtitleLg>I am a software designer and developer living in Houston, Texas. Currently, I am leading the design for our design system at HP and moonlighting as a front-end developer at Paper Crowns.</SubtitleLg>
+  return (
+    <ThemeToggler>
+      {() => (
+      <Wrapper>
+        <SEO title="Home" />
+        <Main>
+
+          <Header>
+            <Container>
+              <nav>
+                <NavLogo href="#">
+                  <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M26.75 0H2C0.89543 0 0 0.89543 0 2V26.5625H26.75C34.0625 26.5625 40 20.625 40 13.3125C40 6 34.0625 0 26.75 0Z"/>
+                    <path d="M0 2.85723V38.0001C0 39.1046 0.895431 40.0001 2 40.0001H36.9822C38.0959 40.0001 38.6536 38.6536 37.8661 37.8662L20 20.0001L0.58 0.590088V0.590088C0.209346 0.947905 0 1.44098 0 1.95616V2.85723Z"/>
+                  </svg>
+                </NavLogo>
+                <NavMenu>
+                  <a href="#">Testing</a>
+                  <a href="#">Testing</a>
+                  <a href="#">Testing</a>
+                </NavMenu>
+              </nav>
+            </Container>
+          </Header>
+
+          <Intro>
+            <Container>
+              <div className="intro-wrapper">
+                <div className="leading">
+                  <TitleLg>Hi, I’m Austin</TitleLg>
+                  <SubtitleLg>I am a software designer and developer living in Houston, Texas. Currently, I am leading the design for our design system at HP and moonlighting as a front-end developer at Paper Crowns.</SubtitleLg>
+                </div>
+                <div className="trailing">
+                  <Img fluid={data.file.childImageSharp.fluid} alt="Austin standing looking relatively happy" draggable="false"/>
+                </div>
               </div>
-              <div className="trailing">
-                <img src="" alt=""/>
-              </div>
-            </div>
-          </Container>
-        </Intro>
+            </Container>
+          </Intro>
 
-        <Featured>
-          <Container>
-            <div className="featured-wrapper">
-              <Title bold>Featured projects</Title>
-              <div className="featured-items">
-                <FeaturedItem>
-                  <div>
-                    <Title>Redesigning and scaling a design system across HP</Title>
-                    <Subtitle>Design Lead<span> • </span>Jul 2019 - Mar 2020</Subtitle>
-                  </div>
-                  <div className="tags">
-                    <Tag>Design</Tag>
-                    <Tag>Design Systems</Tag>
-                  </div>
-                </FeaturedItem>
-                <FeaturedItem>
-                  <div>
-                    <Title>Building a flexible design system to meet varying business needs</Title>
-                    <Subtitle>Design Lead<span> • </span>Jul 2019 - Mar 2020</Subtitle>
-                  </div>
-                  <div className="tags">
-                    <Tag>Design</Tag>
-                    <Tag>Design Systems</Tag>
-                  </div>
-                </FeaturedItem>
+          <Featured>
+            <Container>
+              <div className="featured-wrapper">
+                <Title bold>Featured projects</Title>
+                <div className="featured-items">
+                  <FeaturedItem>
+                    <div>
+                      <Title>Redesigning and scaling a design system across HP</Title>
+                      <Subtitle>Design Lead<span> • </span>Jul 2019 - Mar 2020</Subtitle>
+                    </div>
+                    <div className="tags">
+                      <Tag>Design</Tag>
+                      <Tag>Design Systems</Tag>
+                    </div>
+                  </FeaturedItem>
+                  <FeaturedItem>
+                    <div>
+                      <Title>Building a flexible design system to meet varying business needs</Title>
+                      <Subtitle>Design Lead<span> • </span>Jul 2019 - Mar 2020</Subtitle>
+                    </div>
+                    <div className="tags">
+                      <Tag>Design</Tag>
+                      <Tag>Design Systems</Tag>
+                    </div>
+                  </FeaturedItem>
+                </div>
               </div>
-            </div>
-          </Container>
-        </Featured>
+            </Container>
+          </Featured>
 
-        <CTAs>
-          <Container>
-            <div className="ctas-wrapper">
-              <Title bold>Take a deep dive</Title>
-              <div className="cta-items">
-                <CTAItem>
-                  <div>
-                    <Title>My work</Title>
-                    <Subtitle>Check out the projects that I have worked on, designing and developing software</Subtitle>
-                  </div>
-                  <Button>Check it out</Button>
-                </CTAItem>
-                <CTAItem>
-                  <div>
-                    <Title>My work</Title>
-                    <Subtitle>Check out the projects that I have worked on, designing and developing software</Subtitle>
-                  </div>
-                  <Button>Check it out</Button>
-                </CTAItem>
-                <CTAItem>
-                  <div>
-                    <Title>My work</Title>
-                    <Subtitle>Check out the projects that I have worked on, designing and developing software</Subtitle>
-                  </div>
-                  <Button>Check it out</Button>
-                </CTAItem>
+          <CTAs>
+            <Container>
+              <div className="ctas-wrapper">
+                <Title bold>Take a deep dive</Title>
+                <div className="cta-items">
+                  <CTAItem>
+                    <div>
+                      <Title>My work</Title>
+                      <Subtitle>Check out the projects that I have worked on, designing and developing software</Subtitle>
+                    </div>
+                    <Button>Check it out</Button>
+                  </CTAItem>
+                  <CTAItem>
+                    <div>
+                      <Title>My work</Title>
+                      <Subtitle>Check out the projects that I have worked on, designing and developing software</Subtitle>
+                    </div>
+                    <Button>Check it out</Button>
+                  </CTAItem>
+                  <CTAItem>
+                    <div>
+                      <Title>My work</Title>
+                      <Subtitle>Check out the projects that I have worked on, designing and developing software</Subtitle>
+                    </div>
+                    <Button>Check it out</Button>
+                  </CTAItem>
+                </div>
               </div>
-            </div>
-          </Container>
-        </CTAs>
+            </Container>
+          </CTAs>
 
-        <Footer>
-        </Footer>
-      </Main>
-    </Wrapper>
-    )}
-  </ThemeToggler>
-)
+          <Footer>
+          </Footer>
+        </Main>
+      </Wrapper>
+      )}
+    </ThemeToggler>
+  )
+}
+
+export default Index
